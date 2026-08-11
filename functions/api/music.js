a0b4ba924023f404d75f8b361eb309e2fcb39a79
zaffeine <slappymywangu@gmail.com>
2026-08-11 02:09:43 +0200

    musictrouble3

--- functions/api/music.js
+++ functions/api/music.js
@@ -2,10 +2,11 @@ export async function onRequest(context) {
     const apiKey = context.env.LASTFM_API_KEY;
 
     const url = new URL("https://ws.audioscrobbler.com/2.0/");
-    url.searchParams.set("method", "user.getinfo");
+    url.searchParams.set("method", "user.getrecenttracks");
     url.searchParams.set("user", "zaffeine");
     url.searchParams.set("api_key", apiKey);
     url.searchParams.set("format", "json");
+    url.searchParams.set("limit", "1");
 
     const res = await fetch(url);
     const text = await res.text();
